48e9cac3ef26ec7c987ab552c77ae5fd40bfdc6a
Max Verbeek <m4xv3rb33k@gmail.com>
2015-05-22 22:24:57 +0200

    Finished slash templating engine

--- src/Twist/Slash/Slash.php
+++ src/Twist/Slash/Slash.php
@@ -1,421 +1,183 @@
 <?php namespace Twist\Slash;
 
-use Twist\Slash\Exceptions\SlashSyntaxError;
-use Twist\Slash\Exceptions\IndentationException;
-use Closure;
+use Twist\View\PhpEngine;
+use Twist\View\EngineInterface;
+use Twist\View\ManagerInterface;
 
-class Slash
+class Slash extends PhpEngine implements EngineInterface
 {
 	/**
-	 * A list of functions that compile a template globally
+	 * The Slash compiler
 	 *
-	 * @var  array
+	 * @var \Twist\Slash\Compiler
 	 */
-	protected $globalTasks = [
-		'parseComments',
-	];
+	protected $compiler;
 
 	/**
-	 * A list of functions that compile a line
+	 * A manager that hosts the templating engine
+	 *
+	 * @var \Twist\View\ManagerInterface
+	 */
+	protected $manager;
+
+	/**
+	 * The list of blocks currently being used
 	 *
 	 * @var array
 	 */
-	protected $lineTasks = [
-		'parseLogicStatements',
-		'parseFunctionStatements',
-		'parseEmptyStatements',
-		'parseEndStatements',
-		'parseEchoes',
-	];
+	protected $blocks = [];
 
 	/**
-	 * Tags being used by compiler
-	 *
-	 * @var  array
-	 * @var  array
-	 * @var  array
-	 */
-	protected $echo = ['{{', '}}'];
-	protected $logic = ['{%', '%}'];
-	protected $comment = ['{#', '#}'];
-
-	/**
-	 * Stuff that replaces the end tag
+	 * The list of blocks that are currently being made.
 	 *
 	 * @var array
 	 */
-	protected $endings = [];
+	protected $blockStack = [];
 
-	/**
-	 * Counter to give foreach loops an ID (used for empty statements)
-	 *
-	 * @var integer
-	 */
-	protected $foreachCount = 0;
-
-	/**
-	 * Turns slash code into valid PHP
-	 *
-	 * @param  string $slash Raw slash code
-	 *
-	 * @return string        Valid PHP
-	 */
-	public function make($slash)
+	public function __construct(SlashCompiler $compiler, $cache)
 	{
-		$this->indentation = 0;
-		$this->indentCallbacks = [];
-
-		$result = '';
-
-		foreach (token_get_all($slash) as &$token)
-		{
-			$result .= is_array($token) ? $this->parseToken($token) : $token;
-		}
-
-		return $result;
-	}
-
-	protected function parseToken($token)
-	{
-		list($id, $content) = $token;
-
-		// Fetch all of the non-php code
-		if ($id == T_INLINE_HTML)
-		{
-			$content = $this->parse($content);
-		}
-
-		return $content;
-	}
-
-	protected function parse($content)
-	{
-		$content = $this->parseGlobal($content);
-
-		$lines = preg_split('/[\r\n]/', $content);
-
-		$this->parseExtends($lines);
-
-		return implode(PHP_EOL, $this->parseLines($lines));
-	}
-
-	protected function parseExtends(&$lines)
-	{
-		list($open, $close) = array_map('preg_quote', $this->logic);
-
-		$ws = '[\\t ]*';
-
-		$pattern = "/^{$open}{$ws}extends ((['\"])(.*?)\\2){$ws}{$close}/";
-
-		if (preg_match($pattern, $lines[0], $match))
-		{
-			array_shift($lines);
-			$file = $match[1];
-			$lines[] = "<?php \$__env->make({$file}, get_defined_vars()); ?>";
-		}
+		$this->compiler = $compiler;
+		$this->cache = $cache;
 	}
 
 	/**
-	 * Parse a whole string at once
+	 * Get the evaluated contents of a slash file
 	 *
-	 * @param  string $content
+	 * @param  string $path
+	 * @param  array  $data
 	 *
 	 * @return string
 	 */
-	protected function parseGlobal($content)
+	public function get($path, array $data = array())
 	{
-		if (! $this->parsable($content)) return $content;
+		// Unset some predefined variables, since we may
+		// be processing an included partial. If we don't
+		// do this, __path overwrites the current $__path.
+		if (isset($data['__data'])) unset($data['__data']);
+		if (isset($data['__path'])) unset($data['__path']);
 
-		foreach ($this->globalTasks as $task)
+		$cachefile = $this->getCachePath($path);
+
+		// If the view is modified after the
+		// last compilation it is expired, and must
+		// be recompiled
+		$exists = file_exists($cachefile);
+
+		if (! $exists || $exists && filemtime($path) > filemtime($cachefile))
 		{
-			$content = $this->{$task}($content);
+			$cache = fopen($cachefile, 'w');
+			$view = fopen($path, 'r');
+			fwrite($cache, $this->compiler->compile(fread($view, filesize($path))));
+
+			fclose($cache);
+			fclose($view);
 		}
 
-		return $content;
+		$data['__env'] = $this;
+
+		return parent::get($cachefile, $data);
 	}
 
 	/**
-	 * Parse a string line by line
+	 * Get the location of the cache file
 	 *
-	 * @param  string $content
+	 * @param  string $file
 	 *
 	 * @return string
 	 */
-	protected function parseLines($lines)
+	protected function getCachePath($file)
 	{
-		foreach ($lines as &$line)
+		return $this->cache . '/' . md5($file);
+	}
+
+	/**
+	 * Set a manager for supporting other view engines
+	 *
+	 * @param \Twist\View\ManagerInterface $manager
+	 */
+	public function setManager(ManagerInterface $manager)
+	{
+		$this->manager = $manager;
+	}
+
+	/**
+	 * Retreive another file. Use a different environment if given.
+	 *
+	 * @param  string $file
+	 * @param  array  $data
+	 *
+	 * @return string|\Twist\View\ViewInterface
+	 */
+	public function make($file, $data = array())
+	{
+		if ($this->manager)
 		{
-			if ($this->parsable($line))
-			{
-				foreach ($this->lineTasks as $task)
-				{
-					$line = $this->{$task}($line);
-				}
-			}
+			return $this->manager->make($file, $data);
 		}
 
-		return $lines;
+		// Since all of Twist's views are provided without an extension
+		// we'll have to add our extension here, since slash is the only
+		// language that is supported if no other manager is given.
+		$file = str_replace('.', '/', $file) . SlashCompiler::EXTENSION;
+
+		return $this->get($file, $data);
 	}
 
-	protected function pushEnding($ending)
+	/**
+	 * Begin a new block (compiler output)
+	 *
+	 * @param  string $name
+	 * @param  string $content
+	 *
+	 * @return void
+	 */
+	public function startBlock($name, $content = '')
 	{
-		$this->endings[] = $ending;
-	}
-
-	protected function popEnding()
-	{
-		$ending = array_pop($this->endings);
-
-		if ($ending instanceof Closure)
+		if ($content)
 		{
-			// If the ending is a closure, we'll execute it.
-			// this allows for extra logic being performed
-			// right before the ending is inserted.
-			$ending = $ending();
-		}
-
-		return $ending;
-	}
-
-	protected function parseLogicStatements($line)
-	{
-		list($open, $close) = array_map('preg_quote', $this->logic);
-
-		$callback = function ($matches)
-		{
-			return $this->{'compile'.ucfirst($matches[2]).'Statements'}($matches[1]);
-		};
-
-		return preg_replace_callback("/{$open}[\\t ]*((for|while|if|elseif|else|spaceless)[\\t ]+.*?)[\\t ]*{$close}/", $callback, $line);
-	}
-
-	protected function compileForStatements($statement)
-	{
-		// PHP wiki (language.variables.basic)
-		$var = "\\$[a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*";
-
-		if (preg_match("/for ({$var}) in (\d+)/", $statement, $match))
-		{
-			return $this->compileFor($statement, $match[1], $match[2]);
-		}
-
-		elseif (preg_match($pattern = "/for ({$var})(?:[\\t ]*:[\\t ]*({$var}))? in (.*)/", $statement, $match))
-		{
-			return $this->compileForeach($statement, $match);
+			$this->blocks[$name] = $content;
 		}
 
 		else
 		{
-			echo "Iets is pittig verneukt en het is niet jurryts moeder";
-			echo $statement . '  ' . $pattern;
-			echo PHP_EOL;
+			if (ob_start())	$this->blockStack[] = $name;
 		}
-	}
-
-	protected function compileFor($statement, $var, $to)
-	{
-		$this->pushEnding('<?php endfor; ?>');
-
-		return "<?php for ({$var} = 1; {$var} <= {$to}; {$var}++): ?>";
-	}
-
-	protected function compileForeach($statement, $matches)
-	{
-		if (isset($matches[3])) list(, $key, $value, $array) = $matches;
-
-		else list(, $value, $array) = $matches;
-
-		$empty = $this->createEmptyVariable($this->foreachCount++);
-
-		$this->pushEnding(function () {
-			$this->foreachCount--;
-			return '<?php endforeach; ?>';
-		});
-
-		$iterated = isset($key) ? "{$key} => {$value}" : $value;
-
-		return "<?php {$empty} = true; foreach ({$array} as {$iterated}): {$empty} = false; ?>";
-	}
-
-	protected function compileWhileStatements($statement)
-	{
-		$callback = function ($match)
-		{
-			$this->pushEnding('<?php endwhile; ?>');
-
-			return "<?php while ({$match[1]}): ?>";
-		};
-
-		$pattern = "/while (.*)/";
-
-		return preg_replace_callback($pattern, $callback, $statement);
-	}
-
-	protected function compileIfStatements($statement)
-	{
-		$callback = function ($match)
-		{
-			$this->pushEnding('<?php endif; ?>');
-
-			return "<?php if ({$match[1]}): ?>";
-		};
-
-		$pattern = "/if (.*)/";
-
-		return preg_replace_callback($pattern, $callback, $statement);
-	}
-
-	protected function compileElseifStatements($statement)
-	{
-		return preg_replace("/elseif (.*)/", "<?php elseif (\\1): ?>", $statement);
-	}
-
-	protected function compileElseStatements($statement)
-	{
-		return '<?php else: ?>';
-	}
-
-	protected function compileSpacelessStatements($statement)
-	{
-		$ob_end = "<?php echo trim(preg_replace('/>\s+</', '><', ob_get_clean())); ?>";
-		$this->pushEnding($ob_end);
-
-		return '<?php ob_start(); ?>';
-	}
-
-	protected function parseFunctionStatements($line)
-	{
-		$callback = function ($match)
-		{
-			$function = ucfirst($match[1]);
-			$name     = $match[2] . $match[3] . $match[2];
-			$default  = isset($match[5]) ? $match[4] . $match[5] . $match[4] : '';
-
-			return $this->{"compile{$function}Statements"}($name, $default);
-		};
-
-		list($open, $close) = array_map('preg_quote', $this->logic);
-
-		$ws = '[\\t ]*';
-		$functions = 'yield|block|include';
-
-		$pattern = "/{$open}{$ws}({$functions}){$ws}(['\"])(.*?)\\2{$ws}(?:,{$ws}(['\"])(.*?)\\4)?{$ws}{$close}/";
-
-		return preg_replace_callback($pattern, $callback, $line);
-	}
-
-	protected function compileYieldStatements($name, $default = '')
-	{
-		if (! $default) $default = "''";
-		return "<?php \$__env->yieldBlock({$name}, {$default}); ?>";
-	}
-
-	protected function compileBlockStatements($name, $content = '')
-	{
-		if (! $content)
-		{
-			$content = "''";
-			$ending = "<?php \$__env->endBlock(); ?>";
-			$this->pushEnding($ending);
-
-			return "<?php \$__env->startBlock({$name}, {$content}); ?>";
-		}
-
-		return "<?php \$__env->addBlock({$name}, {$content}); ?>";
-	}
-
-	protected function compileIncludeStatements($name)
-	{
-		return "<?php \$__env->make({$name}, get_defined_vars()); ?>";
-	}
-
-	protected function parseEmptyStatements($line)
-	{
-		$callback = function ($match)
-		{
-			$empty = $this->createEmptyVariable(--$this->foreachCount);
-
-			$this->popEnding();
-			$this->pushEnding('<?php endif; ?>');
-
-			return "<?php endforeach; if ({$empty}): ?>";
-		};
-
-		list($open, $close) = array_map('preg_quote', $this->logic);
-
-		$ws = '[\\t ]*';
-
-		$pattern = "/{$open}{$ws}empty{$ws}{$close}/";
-
-		return preg_replace_callback($pattern, $callback, $line);
-	}
-
-	protected function parseEndStatements($line)
-	{
-		$callback = function ($match)
-		{
-			return $this->popEnding();
-		};
-
-		list($open, $close) = array_map('preg_quote', $this->logic);
-
-		$ws = '[\\t ]*';
-
-		$pattern = "/{$open}{$ws}end{$ws}{$close}/";
-
-		return preg_replace_callback($pattern, $callback, $line);
-	}
-
-	protected function parseEchoes($line)
-	{
-		list($open, $close) = array_map('preg_quote', $this->echo);
-
-		$callback = function ($matches)
-		{
-			$value = preg_replace('/^(?=\$)(.+?)(?:\s+or\s+)(.+?)$/', 'isset($1) ? $1 : $2', $matches[1]);
-
-			return "<?php echo htmlentities({$value}, ENT_QUOTES, \"UTF-8\"); ?>";
-		};
-
-		return preg_replace_callback("/{$open}[\t ]*(.+?)[\t ]*{$close}/", $callback, $line);
-	}
-
-	protected function parseComments($content)
-	{
-		list($open, $close) = array_map('preg_quote', $this->comment);
-
-		$pattern = "/{$open}(.*?){$close}/s";
-
-		return preg_replace($pattern, "<?php /* \\1 */ ?>", $content);
-	}
-
-	protected function parsable($line)
-	{
-		$tags = [];
-
-		foreach ([$this->logic, $this->echo, $this->comment] as $tag)
-		{
-			$tags[] = preg_quote($tag[0], '/') . '|' . preg_quote($tag[1], '/');
-		}
-
-		$tags = implode('|', $tags);
-
-		return preg_match("/{$tags}/", $line, $match) === 1;
 	}
 
 	/**
-	 * If there are many for loops on page
-	 * The index will become negative. Since a '-' is
-	 * not a valid variable character, we replace that here.
-	 *
-	 * @param  integer $index
+	 * End the block (compiler output)
 	 *
 	 * @return string
 	 */
-	protected function createEmptyVariable($index)
+	public function endBlock()
 	{
-		$empty = '$__empty_' . $index;
+		$last = array_pop($this->blockStack);
 
-		return str_replace('-', '_', $empty);
+		if (isset($this->blocks[$last]))
+		{
+			$this->blocks[$last] = preg_replace(
+				$this->compiler->getParentPattern(),
+				$this->blocks[$last],
+				ob_get_clean()
+			);
+		}
+
+		else
+		{
+			$this->blocks[$last] = ob_get_clean();
+		}
+	}
+
+	/**
+	 * Retreive a block
+	 *
+	 * @param  string $block
+	 * @param  string $default
+	 *
+	 * @return string
+	 */
+	public function yieldBlock($block, $default = '')
+	{
+		return isset($this->blocks[$block]) ? $this->blocks[$block] : $default;
 	}
 }
---
+++ src/Twist/Slash/SlashCompiler.php
@@ -0,0 +1,430 @@
+<?php namespace Twist\Slash;
+
+use Twist\Slash\Exceptions\SlashSyntaxError;
+use Twist\Slash\Exceptions\IndentationException;
+use Closure;
+
+class SlashCompiler
+{
+	const EXTENSION = '.slash';
+
+	/**
+	 * A list of functions that compile a template globally
+	 *
+	 * @var  array
+	 */
+	protected $globalTasks = [
+		'parseComments',
+	];
+
+	/**
+	 * A list of functions that compile a line
+	 *
+	 * @var array
+	 */
+	protected $lineTasks = [
+		'parseLogicStatements',
+		'parseFunctionStatements',
+		'parseEmptyStatements',
+		'parseEndStatements',
+		'parseEchoes',
+	];
+
+	/**
+	 * Tags being used by compiler
+	 *
+	 * @var  array
+	 * @var  array
+	 * @var  array
+	 */
+	protected $echo = ['{{', '}}'];
+	protected $logic = ['{%', '%}'];
+	protected $comment = ['{#', '#}'];
+
+	/**
+	 * Stuff that replaces the end tag
+	 *
+	 * @var array
+	 */
+	protected $endings = [];
+
+	/**
+	 * Counter to give foreach loops an ID (used for empty statements)
+	 *
+	 * @var integer
+	 */
+	protected $foreachCount = 0;
+
+	/**
+	 * Turns slash code into valid PHP
+	 *
+	 * @param  string $slash Raw slash code
+	 *
+	 * @return string        Valid PHP
+	 */
+	public function compile($slash)
+	{
+		$this->indentation = 0;
+		$this->indentCallbacks = [];
+
+		$result = '';
+
+		foreach (token_get_all($slash) as &$token)
+		{
+			$result .= is_array($token) ? $this->parseToken($token) : $token;
+		}
+
+		return $result;
+	}
+
+	protected function parseToken($token)
+	{
+		list($id, $content) = $token;
+
+		// Fetch all of the non-php code
+		if ($id == T_INLINE_HTML)
+		{
+			$content = $this->parse($content);
+		}
+
+		return $content;
+	}
+
+	protected function parse($content)
+	{
+		$content = $this->parseGlobal($content);
+
+		$lines = preg_split('/[\r\n]/', $content);
+
+		$this->parseExtends($lines);
+
+		return implode(PHP_EOL, $this->parseLines($lines));
+	}
+
+	protected function parseExtends(&$lines)
+	{
+		list($open, $close) = array_map('preg_quote', $this->logic);
+
+		$ws = '[\\t ]*';
+
+		$pattern = "/^{$open}{$ws}extends ((['\"])(.*?)\\2){$ws}{$close}/";
+
+		if (preg_match($pattern, $lines[0], $match))
+		{
+			array_shift($lines);
+			$file = $match[1];
+			$lines[] = "<?php echo \$__env->make({$file}, get_defined_vars()); ?>";
+		}
+	}
+
+	/**
+	 * Parse a whole string at once
+	 *
+	 * @param  string $content
+	 *
+	 * @return string
+	 */
+	protected function parseGlobal($content)
+	{
+		if (! $this->parsable($content)) return $content;
+
+		foreach ($this->globalTasks as $task)
+		{
+			$content = $this->{$task}($content);
+		}
+
+		return $content;
+	}
+
+	/**
+	 * Parse a string line by line
+	 *
+	 * @param  string $content
+	 *
+	 * @return string
+	 */
+	protected function parseLines($lines)
+	{
+		foreach ($lines as &$line)
+		{
+			if ($this->parsable($line))
+			{
+				foreach ($this->lineTasks as $task)
+				{
+					$line = $this->{$task}($line);
+				}
+			}
+		}
+
+		return $lines;
+	}
+
+	protected function pushEnding($ending)
+	{
+		$this->endings[] = $ending;
+	}
+
+	protected function popEnding()
+	{
+		$ending = array_pop($this->endings);
+
+		if ($ending instanceof Closure)
+		{
+			// If the ending is a closure, we'll execute it.
+			// this allows for extra logic being performed
+			// right before the ending is inserted.
+			$ending = $ending();
+		}
+
+		return $ending;
+	}
+
+	protected function parseLogicStatements($line)
+	{
+		list($open, $close) = array_map('preg_quote', $this->logic);
+
+		$callback = function ($matches)
+		{
+			return $this->{'compile'.ucfirst($matches[2]).'Statements'}($matches[1]);
+		};
+
+		return preg_replace_callback("/{$open}[\\t ]*((for|while|if|elseif|else|spaceless)[\\t ]+.*?)[\\t ]*{$close}/", $callback, $line);
+	}
+
+	protected function compileForStatements($statement)
+	{
+		// PHP wiki (language.variables.basic)
+		$var = "\\$[a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*";
+
+		if (preg_match("/for ({$var}) in (\d+)/", $statement, $match))
+		{
+			return $this->compileFor($statement, $match[1], $match[2]);
+		}
+
+		elseif (preg_match($pattern = "/for ({$var})(?:[\\t ]*:[\\t ]*({$var}))? in (.*)/", $statement, $match))
+		{
+			return $this->compileForeach($statement, $match);
+		}
+
+		else
+		{
+			echo "Iets is pittig verneukt en het is niet jurryts moeder";
+			echo $statement . '  ' . $pattern;
+			echo PHP_EOL;
+		}
+	}
+
+	protected function compileFor($statement, $var, $to)
+	{
+		$this->pushEnding('<?php endfor; ?>');
+
+		return "<?php for ({$var} = 1; {$var} <= {$to}; {$var}++): ?>";
+	}
+
+	protected function compileForeach($statement, $matches)
+	{
+		if (isset($matches[3])) list(, $key, $value, $array) = $matches;
+
+		else list(, $value, $array) = $matches;
+
+		$empty = $this->createEmptyVariable($this->foreachCount++);
+
+		$this->pushEnding(function () {
+			$this->foreachCount--;
+			return '<?php endforeach; ?>';
+		});
+
+		$iterated = isset($key) ? "{$key} => {$value}" : $value;
+
+		return "<?php {$empty} = true; foreach ({$array} as {$iterated}): {$empty} = false; ?>";
+	}
+
+	protected function compileWhileStatements($statement)
+	{
+		$callback = function ($match)
+		{
+			$this->pushEnding('<?php endwhile; ?>');
+
+			return "<?php while ({$match[1]}): ?>";
+		};
+
+		$pattern = "/while (.*)/";
+
+		return preg_replace_callback($pattern, $callback, $statement);
+	}
+
+	protected function compileIfStatements($statement)
+	{
+		$callback = function ($match)
+		{
+			$this->pushEnding('<?php endif; ?>');
+
+			return "<?php if ({$match[1]}): ?>";
+		};
+
+		$pattern = "/if (.*)/";
+
+		return preg_replace_callback($pattern, $callback, $statement);
+	}
+
+	protected function compileElseifStatements($statement)
+	{
+		return preg_replace("/elseif (.*)/", "<?php elseif (\\1): ?>", $statement);
+	}
+
+	protected function compileElseStatements($statement)
+	{
+		return '<?php else: ?>';
+	}
+
+	protected function compileSpacelessStatements($statement)
+	{
+		$ob_end = "<?php echo trim(preg_replace('/>\s+</', '><', ob_get_clean())); ?>";
+		$this->pushEnding($ob_end);
+
+		return '<?php ob_start(); ?>';
+	}
+
+	protected function parseFunctionStatements($line)
+	{
+		$callback = function ($match)
+		{
+			$function = ucfirst($match[1]);
+			$name     = $match[2] . $match[3] . $match[2];
+			$default  = isset($match[5]) ? $match[4] . $match[5] . $match[4] : '';
+
+			return $this->{"compile{$function}Statements"}($name, $default);
+		};
+
+		list($open, $close) = array_map('preg_quote', $this->logic);
+
+		$ws = '[\\t ]*';
+		$functions = 'yield|block|include';
+
+		$pattern = "/{$open}{$ws}({$functions}){$ws}(['\"])(.*?)\\2{$ws}(?:,{$ws}(['\"])(.*?)\\4)?{$ws}{$close}/";
+
+		return preg_replace_callback($pattern, $callback, $line);
+	}
+
+	protected function compileYieldStatements($name, $default = '')
+	{
+		if (! $default) $default = "''";
+		return "<?php echo \$__env->yieldBlock({$name}, {$default}); ?>";
+	}
+
+	protected function compileBlockStatements($name, $content = '')
+	{
+		if (! $content)
+		{
+			$content = "''";
+			$ending = "<?php \$__env->endBlock(); ?>";
+			$this->pushEnding($ending);
+
+			return "<?php \$__env->startBlock({$name}); ?>";
+		}
+
+		return "<?php \$__env->addBlock({$name}, {$content}); ?>";
+	}
+
+	protected function compileIncludeStatements($name)
+	{
+		return "<?php echo \$__env->make({$name}, get_defined_vars()); ?>";
+	}
+
+	protected function parseEmptyStatements($line)
+	{
+		$callback = function ($match)
+		{
+			$empty = $this->createEmptyVariable(--$this->foreachCount);
+
+			$this->popEnding();
+			$this->pushEnding('<?php endif; ?>');
+
+			return "<?php endforeach; if ({$empty}): ?>";
+		};
+
+		list($open, $close) = array_map('preg_quote', $this->logic);
+
+		$ws = '[\\t ]*';
+
+		$pattern = "/{$open}{$ws}empty{$ws}{$close}/";
+
+		return preg_replace_callback($pattern, $callback, $line);
+	}
+
+	protected function parseEndStatements($line)
+	{
+		$callback = function ($match)
+		{
+			return $this->popEnding();
+		};
+
+		list($open, $close) = array_map('preg_quote', $this->logic);
+
+		$ws = '[\\t ]*';
+
+		$pattern = "/{$open}{$ws}end{$ws}{$close}/";
+
+		return preg_replace_callback($pattern, $callback, $line);
+	}
+
+	protected function parseEchoes($line)
+	{
+		list($open, $close) = array_map('preg_quote', $this->echo);
+
+		$callback = function ($matches)
+		{
+			$value = preg_replace('/^(?=\$)(.+?)(?:\s+or\s+)(.+?)$/', 'isset($1) ? $1 : $2', $matches[1]);
+
+			return "<?php echo htmlentities({$value}, ENT_QUOTES, \"UTF-8\"); ?>";
+		};
+
+		return preg_replace_callback("/{$open}[\t ]*(.+?)[\t ]*{$close}/", $callback, $line);
+	}
+
+	protected function parseComments($content)
+	{
+		list($open, $close) = array_map('preg_quote', $this->comment);
+
+		$pattern = "/{$open}(.*?){$close}/s";
+
+		return preg_replace($pattern, "<?php /* \\1 */ ?>", $content);
+	}
+
+	protected function parsable($line)
+	{
+		$tags = [];
+
+		foreach ([$this->logic, $this->echo, $this->comment] as $tag)
+		{
+			$tags[] = preg_quote($tag[0], '/') . '|' . preg_quote($tag[1], '/');
+		}
+
+		$tags = implode('|', $tags);
+
+		return preg_match("/{$tags}/", $line, $match) === 1;
+	}
+
+	public function getParentPattern()
+	{
+		list($open, $close) = array_map('preg_quote', $this->logic);
+
+		return "/{$open}[\\t ]*parent[\\t ]*{$close}/";
+	}
+
+	/**
+	 * If there are many for loops on page
+	 * The index will become negative. Since a '-' is
+	 * not a valid variable character, we replace that here.
+	 *
+	 * @param  integer $index
+	 *
+	 * @return string
+	 */
+	protected function createEmptyVariable($index)
+	{
+		$empty = '$__empty_' . $index;
+
+		return str_replace('-', '_', $empty);
+	}
+}
